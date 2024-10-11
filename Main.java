6f9ac5700077a9549e42a935aca9238b360980f0
tania-n <137098@epsstudents.org>
2024-10-10 20:39:22 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,30 +1,33 @@
 import java.util.Scanner;
 public class Main{
     public static void main(String[] args){
+      // Asks the user to input information that will be used later on 
       Scanner input = new Scanner(System.in) ;
-      System.out.print("Enter your first name ");
+      System.out.print("Enter your first name: ");
       String f_name = input.nextLine();
-      System.out.print("Enter your last name ");
+      System.out.print("Enter your last name: ");
       String l_name = input.nextLine();
       System.out.print("Enter the city  you or one of your parents were born:  ");
       String city = input.nextLine();
-      System.out.print("The name of your grammer school ");
+      System.out.print("The name of your grammer school: ");
       String school = input.nextLine();
-      System.out.print("Enter the first name of a sibling or other relative ");
+      System.out.print("Enter the first name of a sibling or other relative: ");
       String relatives = input.nextLine();
-      System.out.print("Enter the second name of a sibling or other relative  ");
+      System.out.print("Enter the second name of a sibling or other relative:  ");
       String relatives2 = input.nextLine();
-  
-    
-        
+          
         // Sci-fi first name 
         // Concatenates the first 3 letters of the users first name and the first 2 letters of their last name
         String scifiF_name = f_name.substring(0, 3) + l_name.substring(0, 2); 
         // Makes the first letter of the string uppercase and the rest lowercase 
         scifiF_name = scifiF_name.substring(0, 1).toUpperCase() + scifiF_name.substring(1).toLowerCase();
-        //Sci-fi last name
-        String scifi_lname = city.substring(0,2) + school.substring(school.length()-3);
         
+        //Sci-fi last name
+        String scifi_lname = city.substring(0,2) + school.substring(school.length()-3); 
+        // Makes the first letter of the string uppercase and the rest lowercase
+        scifi_lname = scifi_lname.substring(0, 1).toUpperCase() + scifi_lname.substring(1).toLowerCase();
+        
+        //Sci-fi place of origin
         // Generates a two random numbers 
         int firstRand_num = (int)(Math.random() * (relatives.length() - 1)) + 1; 
         int secondRand_num = (int)(Math.random() * (relatives.length() - 1)) + 1;
@@ -36,6 +39,7 @@ public class Main{
         // Makes the first letter of the string uppercase and the rest lowercase
         scifi_origin = scifi_origin.substring(0, 1).toUpperCase() + scifi_origin.substring(1).toLowerCase();
         
+        //Tells the user their Sci-fi name and place of origin 
         System.out.println("Hello " + scifiF_name + " " + scifi_lname + " of " + scifi_origin +". Welcome!" ); 
  
     
